786b2ca258837bc826decaf4bbe66c3f0647512c
LSH-3016 <a673813@naver.com>
2021-07-13 17:16:55 +0900

    2021-07-13 (메인 간결화 및 메소드 추가)

--- src/cal/Cal_main.java
+++ src/cal/Cal_main.java
@@ -16,69 +16,90 @@ import java.util.Scanner;
 
 public class Cal_main {
 	
+	private static final String INPUT_MESSAGE = "식을 입력 해주세요 : ";
+	private static final String OUTPUT_MESSAGE = "계산 결과 : ";
+	private static final String SPERATOR = " ";
+	private static final Scanner scanner = new Scanner(System.in);
+	
+	private static final String PLUS = "+";
+	private static final String SUBTRACTION = "-";
+	private static final String MULTIPLY = "*";
+	private static final String DIVISION = "/";
+	
+	private static ArrayList<String> number = new ArrayList<String>();
+	private static ArrayList<String> operator = new ArrayList<String>();
+	
+
+	private static int RESULT;
+	
 	public static void main(String[] args) {
 		
 		/*		 식 입력 받음		*/
-		String input = input();
+		String input = Input();
 		
 		/*		띄어쓰기로 구분		*/
-		String temp[] = input.split(" "); //스플릿해서 temp에 저장
+		String splited[] = split(input); //스플릿해서 저장
 		
 		/*		숫자와 연산자 구분	*/
-		ArrayList num = new ArrayList();
-		ArrayList oper = new ArrayList();
-		
-		for(int i = 0; i < temp.length; i++) {
-			if (div(temp[i]).equals("oper")) { // 해당 값이 oper라고 판명나면 oper리스트에 저장
-				oper.add(temp[i]);
-			}
-			else if (div(temp[i]).equals("num")) { // 해당 값이 oper외의 값이라면 num으로 판명하여 num에 저장
-				num.add(temp[i]);
-			}
+		for (int i = 0; i < splited.length; i++) {
+			addData(splited[i]);
 		}
 		
 		/* 			식 계산		*/
-		try { // 숫자 이외의 값을 거르기 위해 try사용
-			int result = (Integer.parseInt((String) num.get(0))); // 첫번째 숫자로 초기값 설정
-			
-			for(int i = 0; i < oper.size(); i++){
-				result = cal((oper.get(i)).toString(), Integer.parseInt((num.get(i + 1)).toString()), result);
-			}
-			
-		/*			결과 출력		*/
-			System.out.print("결과 : " + result);
-			
-		} catch (Exception e) { // catch를 이용하여 숫자 이외의 값이 전달되면 오류 표시
-			System.out.println("잘못된 값을 입력하셨습니다.");
-			e.getStackTrace();
+		RESULT = parseInt(number.get(0)); // number의 첫번째값으로 결과값 초기설정
+		for (int i = 0; i < operator.size(); i++) {
+			Caculating(operator.get(i),parseInt(number.get(i+1)));
 		}
+		
+		Output();
 		
 	}
 	
-	public static String input() { // 값 입력받는 메소드
-		String str = new String();
-		Scanner scan = new Scanner(System.in);
-		System.out.print("입력 : ");
-		str = scan.nextLine();
-		return str;
+	public static String Input() { // 값 입력받는 메소드
+		System.out.print(INPUT_MESSAGE);
+		return scanner.nextLine();
 	}
 	
-	public static String div(String tmp) { // 연산자와 숫자를 구분하는 메소드
-		if(tmp.equals("+") || tmp.equals("-") || tmp.equals("*") || tmp.equals("/")) {
-			return "oper"; // 연산자면 oper를 반환
-		}
-		else {
-			return "num"; // 연산자외 값은 모두 num으로 반환
+	public static String[] split(String expression) { // 지정된 문자 기준으로 문자열을 잘라 반환하는 메소드
+		return expression.split(SPERATOR);
+	}
+	
+	public static void addData(String splited) {
+		if(isNumber(splited) == true)
+			number.add(splited);
+		else
+			operator.add(splited);
+	}
+	
+	public static boolean isNumber(String splited) { // 연산자와 숫자를 구분하는 메소드
+		try{
+			Integer.parseInt(splited);
+			return true;
+		} catch(NumberFormatException e) {
+			return false;
 		}
 	}
+	
+	public static int parseInt(String string) {
+		return Integer.parseInt(string);
+	}
 
-	public static int cal (String oper, int num, int result) { // 연산자에 따른 계산을 위한 메소드
-		switch(oper) {
-		case "+": // +일때 처리
-			return result + num;
-		case "-": // -일떄 처리
-			return result - num;
-		}
-		return -99999;
+	public static void Caculating (String oper, int num) { // 연산자에 따른 계산을 위한 메소드
+		if(oper.equals(PLUS))
+			doPlus(num);
+		else if (oper.equals(SUBTRACTION))
+			doSubtraction(num);
+	}
+	
+	public static void doPlus(int num) {
+		RESULT += num;
+	}
+	
+	public static void doSubtraction(int num) {
+		RESULT -= num;
+	}
+	
+	public static void Output() {
+		System.out.print(OUTPUT_MESSAGE + RESULT);
 	}
 }
