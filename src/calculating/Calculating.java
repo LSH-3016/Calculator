c07b20e156d99ca4efa38d9d717ac172da8979f9
LSH-3016 <a673813@naver.com>
2021-08-31 16:23:23 +0900

    로직 및 객체 사용 변경

--- src/calculating/Calculating.java
+++ src/calculating/Calculating.java
@@ -1,39 +1,35 @@
 package calculating;
 
 import calculator.Calculator;
+import calculator.Operator;
+import calculator.Number;
 
 public class Calculating {
 
 	private final char PLUS = '+';
 	private final char MINUS = '-';
-	private final String ERROR_MESSAGE = "잘못된 값을 입력하셨습니다.";
 
 	public void doCalculating(Calculator calculator) {
-		calculator.setResult(calculator.number.poll()); // 첫 숫자값으로 초기값 설정
+		calculator.setResult(calculator.pollNumber().getNumber()); // 첫 숫자값으로 초기값 설정
 		while (!calculator.operator.isEmpty()) {
-			checkingOperator(calculator, calculator.operator.poll());
+			checkingOperator(calculator, calculator.pollOperator());
 		}
 	}
 
-	private void checkingOperator(Calculator calculator, String operator) {
-		try {
-			if ((operator.charAt(0)) == PLUS) {
-				doPlus(calculator);
+	private void checkingOperator(Calculator calculator, Operator operator) {
+			if (operator.getOperator() == PLUS) {
+				doPlus(calculator, calculator.pollNumber());
 			}
-			if ((operator.charAt(0)) == MINUS) {
-				doSubtraction(calculator);
+			if ((operator.getOperator()) == MINUS) {
+				doSubtraction(calculator, calculator.pollNumber());
 			}
-		} catch (Exception e) {
-			e.getStackTrace();
-			System.out.println(ERROR_MESSAGE);
-		}
 	}
 	
-	private void doPlus(Calculator calculator) {
-		calculator.setResult(calculator.result + calculator.number.poll());
+	private void doPlus(Calculator calculator, Number number) {
+		calculator.setResult(calculator.getResult() + number.getNumber());
 	}
 
-	private void doSubtraction(Calculator calculator) {
-		calculator.setResult(calculator.result - calculator.number.poll());
+	private void doSubtraction(Calculator calculator, Number number) {
+		calculator.setResult(calculator.getResult() - number.getNumber());
 	}
 }
