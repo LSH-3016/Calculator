41801432e7b9cf1beb20921d2950e6b8da1b5da2
LSH-3016 <a673813@naver.com>
2021-08-31 16:23:56 +0900

    객체를 사용하도록 메소드 변경

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -9,17 +9,33 @@ import calculating.Calculating;
 public class Calculator {
 	public int result;
 	
-	public Queue<Integer> number = new LinkedList<>();
-	public Queue<String> operator = new LinkedList<>();
+	public Queue<Number> number = new LinkedList<>();
+	public Queue<Operator> operator = new LinkedList<>();
 	
 	public void run() {
-		new Input().inputData(this);
+		new Input(this);
 		new Calculating().doCalculating(this);
-		new Output().outputData(this);
+		new Output(this);
+	}
+		
+	public Number pollNumber() {
+		return number.poll();
 	}
 	
-	public void setResult(int result) {
-		this.result = result;
+	public void setNumber(Number number) {
+		this.number.add(number);
+	}
+	
+	public Operator pollOperator() {
+		return operator.poll();
+	}
+	
+	public void setOperator(Operator operator) {
+		this.operator.add(operator);
+	}
+	
+	public void setResult(int number) {
+		this.result = number;
 	}
 	
 	public int getResult() {
