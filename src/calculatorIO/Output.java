6254e88ba6d59557711b6586c248341c162e1ea1
LSH-3016 <a673813@naver.com>
2021-08-31 16:18:24 +0900

    객체 형태로 변경

--- src/calculatorIO/Output.java
+++ src/calculatorIO/Output.java
@@ -5,7 +5,7 @@ import calculator.Calculator;
 public class Output {
 	private final String OUTPUT_MESSAGE = "계산 결과 : ";
 	
-	public void outputData(Calculator caculator) {
+	public Output(Calculator caculator) {
 		System.out.println(OUTPUT_MESSAGE + caculator.getResult());
 	}
 }
